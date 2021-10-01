3a192ec5f756464d0ba514f64c622809abd2224f
navigaid <navigaid@gmail.com>
2021-10-01 14:52:29 +0800

    nocss

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import React from "react";
 import term from './term.svg';
-import "./App.css";
 
 export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={term} className="App-term" alt="term" />
+    <div>
+      <header>
+        <img src={term} alt="term" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
           href="https://github.com/btwiuse/react-esbuild-starter"
           target="_blank"
           rel="noopener noreferrer"
