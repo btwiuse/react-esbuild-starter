2470a9b7e0fb1c4f442be55f06f7c68595086281
navigaid <navigaid@gmail.com>
2023-01-14 02:30:22 +0800

    add ufo-index

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,80 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useSWR, { mutate } from "swr";
 import term from "./term.svg";
 import "./App.css";
+import DataTable from "react-data-table-component";
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
+
+interface Row {
+  host: string;
+  created_at: string;
+  updated_at?: string;
+}
+
+function Profile() {
+  const { data, error, isLoading } = useSWR("//ufo.k0s.io/", fetcher);
+
+  useEffect(() => {
+    setInterval(() => {
+      mutate("//ufo.k0s.io/");
+      console.log(new Date(), "refreshing...");
+    }, 5000);
+  }, []);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  // console.table(data.map(x=>[x.host,x.created_at]));
+  console.table(data.map((x: Row) => {
+    return {
+      host: "https://" + x.host,
+      createdAt: x.created_at,
+      updatedAt: new Date(),
+    };
+  }));
+  const columns = [
+    {
+      name: "host",
+      selector: (x: Row) => x.host,
+      cell: (x: Row) => <a href={"https://" + x.host}>{x.host}</a>,
+      sortable: true,
+    },
+    {
+      name: "created at",
+      selector: (x: Row) => x.created_at,
+      cell: (x: Row) => <p>{(new Date(x.created_at)).toLocaleString()}</p>,
+      sortable: true,
+    },
+    {
+      name: "updated at",
+      selector: (x: Row) => x.updated_at!,
+      cell: (x: Row) => <p>{(new Date(x.updated_at!)).toLocaleString()}</p>,
+      sortable: true,
+    },
+  ];
+  // console.table(data);
+  return (data.length
+    ? (
+      <DataTable
+        columns={columns}
+        data={data.sort((x: Row, y: Row) =>
+          x.created_at > y.created_at ? -1 : 1
+        ).map((x: Row) => {
+          return {
+            ...x,
+            updated_at: new Date(),
+          };
+        })}
+      />
+    )
+    : null);
+}
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={term} className="App-term" alt="term" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/btwiuse/react-esbuild-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          btwiuse/react-esbuild-starter
-        </a>
+        <Profile />
       </header>
     </div>
   );
